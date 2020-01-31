5ac0593b3b72705b947985f2b6a36d3be01d8dd4
solov <3949058+SOLEROM@users.noreply.github.com>
2020-01-31 14:40:27 +0200

    auto update

--- tornado/proj1/bash1.sh
+++ tornado/proj1/bash1.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-ls /
+
+i="0"
+
+while [ $i -lt 5 ]
+do
+echo  $i
+i=$[$i+1]
+sleep 1
+done
+
--- tornado/proj1/run1.py
+++ tornado/proj1/run1.py
@@ -1,15 +1,61 @@
 import os
-import tornado.ioloop
+import logging
+
 import tornado.web
-import subprocess
+import tornado.websocket
+import tornado.httpserver
+import tornado.ioloop
+
+
+from subprocess import Popen, PIPE, STDOUT
+from tornado.process import Subprocess
+
+
+from tornado.options import define, options
+
 
 static_path_dir=os.path.join(os.path.dirname(__file__), 'static')
 
 class call1(tornado.web.RequestHandler):
-	def get(self):
-		print "Led ON1"
-		self.write("Led ON2")
-                subprocess.call("./bash1.sh", shell=True)
+    def get(self):
+        logging.info("call1::get")
+        self.write("self.write=msg1")
+        ##subprocess stdout print
+        p=Popen("./bash1.sh", shell=True,stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
+        output = p.stdout.read()
+        self.write(output)
+
+class call2(tornado.websocket.WebSocketHandler):
+    def open(self):
+         logging.info("call2::open")
+         filename = "/tmp/b"
+         self.proc = Subprocess(["tail", "-f", filename, "-n", "0"],stdout=Subprocess.STREAM,bufsize=1)
+         self.proc.set_exit_callback(self._close)
+         self.proc.stdout.read_until("\n", self.write_line)
+
+    def _close(self, *args, **kwargs):
+        self.close()
+
+    def on_close(self, *args, **kwargs):
+        logging.info("trying to kill process")
+        self.proc.proc.terminate()
+        self.proc.proc.wait()
+
+    def write_line(self, data):
+       logging.info("Returning to client: %s" % data.strip())
+       self.write_message(data.strip() + "<br/>")
+       self.proc.stdout.read_until("\n", self.write_line)
+
+
+class call3(call2):
+    def open(self):
+         print "call3::open"
+         self.proc = Subprocess(["./bash1.sh"],stdout=Subprocess.STREAM,bufsize=1)
+         ## if subprocess is ending no need for exit callback!!!
+         #self.proc.set_exit_callback(self._close)
+         self.proc.stdout.read_until("\n", self.write_line)
+
+
 
 class MainHandler1(tornado.web.RequestHandler):
     def get(self):
@@ -21,6 +67,8 @@ class MainHandler1(tornado.web.RequestHandler):
 def make_app():
     return tornado.web.Application([
         (r"/call1", call1),
+        (r"/call2", call2),
+        (r"/call3", call3),
         (r'/static/(.*)', tornado.web.StaticFileHandler,  {'path': static_path_dir} ),
         (r"/", MainHandler1),
     ])
@@ -28,4 +76,6 @@ def make_app():
 if __name__ == "__main__":
     app = make_app()
     app.listen(8888)
+    logging.basicConfig(level=logging.DEBUG)
+    logging.info("main started")
     tornado.ioloop.IOLoop.current().start()
